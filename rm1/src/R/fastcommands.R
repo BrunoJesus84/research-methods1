1b96861b744c22b4377ad6f12c067824e9ff19e0
macruz <magnus.cruz@criticalsoftware.com>
2019-10-28 15:40:45 +0000

    more commands

--- rm1/src/R/fastcommands.R
+++ rm1/src/R/fastcommands.R
@@ -13,3 +13,42 @@ s
 # matrix
 m <- matrix(data = 1:6, nrow =2, ncol = 3)
 m
+
+# Creating an array
+a <- array(data = 1:8, dim = c(2,2,2))
+a
+
+# Creating a list
+l <- list(TRUE, 1L, 2.34, "abc")
+
+# Creating a set of factors
+factors <- factor(c("Male", "Female", "Male", "Female", "Male"))
+levels(factors)
+unclass(factors)
+
+# Creating a data table
+dt <- data.frame(
+  Name = c("Cat", "Dog", "Cow", "Pig"),
+  HowMany = c(5, 10, 15, 20),
+  IsPet = c(TRUE, TRUE, FALSE, FALSE)
+)
+dt
+
+# Indexing data Row=1, Col=2
+dt[1, 2]
+
+# Indexing data Row=1
+dt[1, ]
+
+# Indexing data Col=2
+dt[, 2]
+
+# Indexing data by Column name
+dt[["HowMany"]]
+dt$HowMany
+
+# Subsetting/Filtering data
+dt[c(2,4),]
+dt[2:4,]
+dt[dt$IsPet == TRUE,]
+dt[dt$HowMany > 10,]
